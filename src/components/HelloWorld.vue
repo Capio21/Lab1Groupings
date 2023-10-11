12db5384e434730307dd98cfad70b7c7eab5a068
Johnloydgutierrez <gutierrezjl750@gmail.com>
2023-10-11 17:48:08 +0800

    New Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,26 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light fixed-top">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">MocoFaco</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/manage">Manage</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Option
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Add to cart</a></li>
-            <li><a class="dropdown-item" href="#">Order Receive</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Other</a></li>
-          </ul>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Link</a>
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
-<div class="container">
-  <div class="row justify-content-center align-items-center" style="height: 100vh;">
-    <div class="col-12 text-center">
-      <p class="fs-1">THIS IS OUR HOME PAGE</p>
-      <h3>click the manage button</h3>
-    </div>
-  </div>
-</div>
-
-
 </template>
